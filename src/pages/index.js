3d9119358b952a5f4472cc42abb4730e7f77f1a7
Jeremy-Logan <44982128+Jeremy-Logan@users.noreply.github.com>
2020-10-07 13:26:36 -0700

    Fun site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,80 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+// import Image from '../components/image'
+import SEO from '../components/seo'
+import { Heading, Container, Text, Grid, Card, Box, Image, Button, useColorMode } from 'theme-ui'
 
-const IndexPage = () => (
+const IndexPage = () => {
+const[colorMode, setColorMode] = useColorMode()
+
+  return (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Button onClick={(e) => {
+      setColorMode(colorMode === 'default' ? 'dark' : 'default')
+    }}>
+    Toggle {colorMode === 'default' ? 'Dark' : 'Light'}
+    </Button>
+    <Container>
+      <Heading as="h1"> Very Good Fruit Store</Heading>
+      <Grid columns={[1, 2, 3]} gap={20}>
+        <Card>
+          <Heading>Fresh</Heading>
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+            tempor urna in orci porttitor, sit amet molestie lorem imperdiet.
+            Cras fermentum arcu nec ante egestas, sit amet rhoncus nunc finibus.
+            Pellentesque eget dapibus velit.
+          </Text>
+        </Card>
+        <Card>
+          <Heading>Tasty</Heading>
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+            tempor urna in orci porttitor, sit amet molestie lorem imperdiet.
+            Cras fermentum arcu nec ante egestas, sit amet rhoncus nunc finibus.
+            Pellentesque eget dapibus velit.
+          </Text>
+        </Card>
+        <Card>
+          <Heading>Organic</Heading>
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+            tempor urna in orci porttitor, sit amet molestie lorem imperdiet.
+            Cras fermentum arcu nec ante egestas, sit amet rhoncus nunc finibus.
+            Pellentesque eget dapibus velit.
+          </Text>
+        </Card>
+      </Grid>
+      <Grid columns={[1,2]}>
+      <Box>
+      <Image src={'https://www.fillmurray.com/400/300'
+      } style={{backgroundSize: 'contain'}}/>
+      </Box>
+      <Heading>This Store is Great!</Heading>
+      <Text variant={'text.caps'}> 
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+            tempor urna in orci porttitor, sit amet molestie lorem imperdiet.
+            Cras fermentum arcu nec ante egestas, sit amet rhoncus nunc finibus.
+            Pellentesque eget dapibus velit.
+      </Text>
+      <Heading>This Store is Great!</Heading>
+      <Text> 
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+            tempor urna in orci porttitor, sit amet molestie lorem imperdiet.
+            Cras fermentum arcu nec ante egestas, sit amet rhoncus nunc finibus.
+            Pellentesque eget dapibus velit.
+      </Text>
+      <Box>
+      <Image src={'https://www.fillmurray.com/400/300'
+      } style={{backgroundSize: 'contain'}}/>
+      </Box>
+      </Grid>
+    </Container>
   </Layout>
 )
+}
 
 export default IndexPage
